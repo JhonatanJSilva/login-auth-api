b60d2cfff1a1a85add2ee99c61ea681e56956799
Jhonatan Silva <jjsilvajlle@gmail.com>
2024-11-08 12:03:42 -0300

    Fine adjustments to integrate the back end with the front end

--- src/main/java/com/example/login_auth_api/controllers/UserController.java
+++ src/main/java/com/example/login_auth_api/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.example.login_auth_api.controllers;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
